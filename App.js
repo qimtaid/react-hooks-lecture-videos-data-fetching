17673153a0d3de2c2114f8b2bc7fcc56c334ad26
Dan Koskei <qimtaid@gmail.com>
2024-05-01 20:41:53 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import FoxImage from "./FoxImage"
+
+function App() {
+  return (
+    <div>
+      <h1>🦊 FoxFindr 🦊</h1>
+      <FoxImage />
+    </div>
+  );
+}
+
+export default App
